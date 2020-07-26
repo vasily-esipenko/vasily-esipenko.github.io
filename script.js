6f65863e2685edb9f88a3a801518560df959548a
vasily-esipenko <esipenkovasilij@gmail.com>
2020-07-26 11:44:46 +0300

    Update script.js; Add Vue

---
+++ script.js
@@ -0,0 +1,43 @@
+var main = new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            {
+                title: "BMI-calculator",
+                description: "Python console program, which calculates body mass index in various measuring systems.", 
+                github: "https://github.com/vasily-esipenko/BMI-Calculator",
+                link: "https://github.com/vasily-esipenko/BMI-Calculator"
+            },
+            {
+                title: "LinkBot",
+                description: "Telegram bot which creates links to social network profiles out of the sent text.", 
+                github: "https://github.com/vasily-esipenko/LinkBot",
+                link: "https://t.me/firstlink1_bot" 
+            },
+            {
+                title: "CurrencyBot",
+                description: "Telegram Bot that shows currency and bitcoin rates in real time using Aiogram and SQLite.", 
+                github: "https://github.com/vasily-esipenko/CurrencyBot",
+                link: "https://t.me/currencytgbot" 
+            },
+            {
+                title: "WeatherBot",
+                description: "Telegram bot that shows weatger in various cities and countries using TelegramBot API.", 
+                github: "https://github.com/vasily-esipenko/WeatherBot",
+                link: "https://t.me/weather4youbot"
+            },
+            {
+                title: "Vue.js Calculator",
+                description: "A simple Vue.js arithmetic calculator.", 
+                github: "https://github.com/vasily-esipenko/vue-calculator",
+                link: "https://vue-calc2020.web.app/" 
+            },
+            {
+                title: "Vue.js Todo List",
+                description: "Todo List web application with Vue.js", 
+                github: "https://github.com/vasily-esipenko/vue-todolist",
+                link: "https://todo-vue2020.web.app/" 
+            }
+        ]
+    }
+});
